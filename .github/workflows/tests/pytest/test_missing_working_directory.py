bcdf2cfe307aff7c60e74ddeb30a836a59be66ae
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-17 10:48:13 +0000

    tweak

--- .github/workflows/tests/pytest/test_missing_working_directory.py
+++ .github/workflows/tests/pytest/test_missing_working_directory.py
@@ -2,5 +2,6 @@ from utils.act_test_utils import run_act_workflow, assert_output_contains, get_w
 
 def test_missing_working_directory():
     # logs = run_act_workflow("test-missing-working-directory")
-    logs = get_workflow_logs("test-missing-working-directory")
+    with open("workflow.log") as f:
+        logs = f.read()
     assert_output_contains(logs, "Missing required input: working-directory")
