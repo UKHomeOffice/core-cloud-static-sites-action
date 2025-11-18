948e06b5701919ac5755d7d7ca7f97a73b9a65fd
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-18 14:06:28 +0000

    tweak

--- .github/workflows/tests/pytest/test_verify_deleted_file.py
+++ .github/workflows/tests/pytest/test_verify_deleted_file.py
@@ -2,8 +2,8 @@ from utils.act_test_utils import trigger_workflow, create_file, assert_file_in_s
 
 def test_verify_deleted_file():
     # create_file("delete.txt")
-    assert_file_in_folder("delete.txt", should_exist=True)
     trigger_workflow("test-run-action.yml", "CCL-763-testing")
+    assert_file_in_folder("delete.txt", should_exist=True)
     assert_file_in_s3("delete.txt", should_exist=True)
     delete_file("delete.txt")
     assert_file_in_folder("delete.txt", should_exist=False)
