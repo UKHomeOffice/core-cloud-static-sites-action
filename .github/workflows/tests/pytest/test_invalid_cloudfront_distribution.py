1cb9ed45cb6009906407e670c66323637961e469
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-18 16:03:06 +0000

    remove branch reference

--- .github/workflows/tests/pytest/test_invalid_cloudfront_distribution.py
+++ .github/workflows/tests/pytest/test_invalid_cloudfront_distribution.py
@@ -1,6 +1,6 @@
 from utils.act_test_utils import trigger_workflow, assert_output_contains, fetch_logs
 
 def test_invalid_cloudfront_distribution():
-    run_id = trigger_workflow("test-invalid-cloudfront-distribution.yml", "CCL-763-testing")
+    run_id = trigger_workflow("test-invalid-cloudfront-distribution.yml")
     logs = fetch_logs(run_id)
     assert_output_contains(logs, "An error occurred (AccessDenied) when calling the CreateInvalidation operation")
