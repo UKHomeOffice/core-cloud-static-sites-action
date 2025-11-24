adc4520f4c9d2c6d784fddcd6814e8819a08d748
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-24 09:52:20 +0000

    errors

--- .github/workflows/tests/pytest/test_missing_bucket_name.py
+++ .github/workflows/tests/pytest/test_missing_bucket_name.py
@@ -1,6 +1,11 @@
 from utils.act_test_utils import trigger_workflow, assert_output_contains, fetch_logs
+import json
 
 def test_missing_bucket_name():
     run_id = trigger_workflow("test-missing-bucket-name.yml", "CCL-763-testing")
     logs = fetch_logs(run_id)
-    assert_output_contains(logs, "Missing required input: bucket-name")
+
+    with open("expected_errors.json", "r") as f:
+        expected_errors = json.load(f)
+
+    assert_output_contains(logs, expected_errors["missing_bucket"])
