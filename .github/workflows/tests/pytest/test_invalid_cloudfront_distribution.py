c6309af874c6d67a80020c758f5ddb0c4dba8b90
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-24 11:36:42 +0000

    next

--- .github/workflows/tests/pytest/test_invalid_cloudfront_distribution.py
+++ .github/workflows/tests/pytest/test_invalid_cloudfront_distribution.py
@@ -1,6 +1,9 @@
-from utils.act_test_utils import trigger_workflow, assert_output_contains, fetch_logs
+from utils.act_test_utils import trigger_workflow, assert_output_contains, fetch_logs, get_errors
 
 def test_invalid_cloudfront_distribution():
     run_id = trigger_workflow("test-invalid-cloudfront-distribution.yml", "CCL-763-testing")
     logs = fetch_logs(run_id)
-    assert_output_contains(logs, "An error occurred (AccessDenied) when calling the CreateInvalidation operation")
+
+    expected_errors = get_errors()
+    
+    assert_output_contains(logs, expected_errors["invalid_cloudfront_distribution"])
