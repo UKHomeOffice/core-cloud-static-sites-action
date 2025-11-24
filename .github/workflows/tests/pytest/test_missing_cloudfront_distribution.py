5cb18f70c53b8197bf3cde28f4bf626bd86de185
Chris Freeman <christopher.freeman@digital.homeoffice.gov.uk>
2025-11-24 15:55:44 +0000

    chrismessabout3

--- .github/workflows/tests/pytest/test_missing_cloudfront_distribution.py
+++ .github/workflows/tests/pytest/test_missing_cloudfront_distribution.py
@@ -1,7 +1,7 @@
 from utils.test_utils import trigger_workflow, assert_output_contains, fetch_logs, get_errors
 
 def test_missing_cloudfront_distribution():
-    run_id = trigger_workflow("test-missing-cloudfront-distribution.yml", "CCL-763-testing")
+    run_id = trigger_workflow("test-missing-cloudfront-distribution.yml", "chrismessabout3")
     logs = fetch_logs(run_id)
 
     expected_errors = get_errors()
