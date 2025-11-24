a27461cb84ef4f51632edf8fdb2811ab058c4e71
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2025-11-24 09:59:01 +0000

    path

--- .github/workflows/tests/pytest/test_missing_bucket_name.py
+++ .github/workflows/tests/pytest/test_missing_bucket_name.py
@@ -1,11 +1,14 @@
 from utils.act_test_utils import trigger_workflow, assert_output_contains, fetch_logs
 import json
+import os
 
 def test_missing_bucket_name():
     run_id = trigger_workflow("test-missing-bucket-name.yml", "CCL-763-testing")
     logs = fetch_logs(run_id)
 
-    with open("./expected_errors.json", "r") as f:
+    file_path = os.path.join(os.path.dirname(__file__), "expected_errors.json")
+
+    with open(file_path, "r") as f:
         expected_errors = json.load(f)
 
     assert_output_contains(logs, expected_errors["missing_bucket"])
